059235dcb2a0958119001b3ddad70f8277049415
Colton Dowling <Colton@cd3style.com>
2017-09-13 13:41:38 -0500

    boiler plate

---
+++ earthquake-scaffold/src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="title">
+          <div className="my-header">
+            <h1>EarthQuakes</h1>
+
+          </div>
+        </div>
+
+        <p className="App-intro">
+          
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
